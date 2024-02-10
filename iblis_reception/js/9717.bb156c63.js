6676e6c582c458131c78193d4457be94894744ff
Hopson Gausi <hopgausi20@gmail.com>
2024-02-10 10:56:19 +0200

    update iblis reception

---
+++ iblis_reception/js/9717.bb156c63.js
@@ -0,0 +1,5 @@
+"use strict";(globalThis["webpackChunkIBLISReception"]=globalThis["webpackChunkIBLISReception"]||[]).push([[9717],{9717:(e,i,a)=>{a.r(i),a.d(i,{g:()=>h});var c=a(6727),h=c.i;
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */}}]);
+//# sourceMappingURL=9717.bb156c63.js.map
